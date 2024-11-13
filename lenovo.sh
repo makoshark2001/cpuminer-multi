bc00a6c56a4f1283bd2d88fb6951efc2c2bacb77
HTMauney <makoshark2001@gmail.com>
2024-11-13 15:30:36 -0500

    Update lenovo.sh

--- lenovo.sh
+++ lenovo.sh
@@ -4,10 +4,8 @@ git clone https://github.com/tpruvot/cpuminer-multi
 
 sudo apt-get -y install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
 
-./build.sh
-
 cd cpuminer-multi
-
+./build.sh
 
 
 
