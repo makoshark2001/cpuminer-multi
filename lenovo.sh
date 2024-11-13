28aeccc17796b76d35d65b32bbd81c6b4e1f0f06
HTMauney <makoshark2001@gmail.com>
2024-11-13 15:37:03 -0500

    Update lenovo.sh

--- lenovo.sh
+++ lenovo.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-git clone https://github.com/tpruvot/cpuminer-multi
-
-sudo apt-get -y install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
-
-cd ~/cpuminer-multi
-
-./build.sh
-
-
 
 cat << EOF > ~/cpuminer-multi/scrypt.sh
 #!/bin/sh
