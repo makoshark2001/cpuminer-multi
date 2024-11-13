99937c7f780e47947d92e4ac872bded1f0228fd1
HTMauney <makoshark2001@gmail.com>
2024-11-13 16:23:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
 
+wget https://raw.githubusercontent.com/makoshark2001/cpuminer-multi/main/sha256d.sh -O ~/cpuminer-multi/sha256d.sh
+
+cat << EOF > ~/cpuminer-multi/sha256d-screen.sh
+#!/bin/sh
+screen -S Sha256d -d -m ./sha256d.sh
+EOF
+
+
+
+chmod +x *.sh
